b068a9580d1eb2f9caebba76a17723071e3e7151
silseva <silseva@fastwebnet.it>
2019-05-03 09:07:54 +0200

    Code for led blink example

--- src/1.led_blink/main.cpp
+++ src/1.led_blink/main.cpp
@@ -5,7 +5,33 @@
 using namespace std;
 using namespace miosix;
 
+// Create an easy-to-use label for the GPIO connected to PORT A, line 5 (also
+// indicated with PA5).
+// In this case it corresponds with the on-board green led.
+
+typedef Gpio<GPIOA_BASE, 5> led;
+
+// An alternative form of declaration, more C++-ish can be the following:
+// using led = Gpio<GPIOA_BASE, 5>;
+
 int main()
 {
-    //iprintf("Hello world, write your application here\n");
+    // Set the GPIO line in output mode
+    led::mode(Mode::OUTPUT);
+
+    while(1)
+    {
+        // Put the GPIO line in high state (turn on the led)
+        led::high();
+
+        // Wait for 500 milliseconds. One can also use the function delayMs()
+        Thread::sleep(500);
+
+        // Put the GPIO line in low state (turn off the led)
+        led::low();
+
+        Thread::sleep(500);
+    }
+
+    return 0;
 }
